bc8578b7be3fa39c7caae07d942868a118caa408
David La Grange <davidnlagrange@hotmail.com>
2021-05-12 13:47:54 -0500

    Added Post Model and ApplicationDbContext Context Class

---
+++ SocialMediaWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ SocialMediaWebAPI/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaWebAPI.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Title is a required field.")]
+        [MaxLength(60, ErrorMessage ="Title cannot exceed 60 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Content is a required field.")]
+        [MaxLength(500, ErrorMessage = "Content cannot exceed 500 characters")]
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime PostDate { get; set; }
+    }
+}
