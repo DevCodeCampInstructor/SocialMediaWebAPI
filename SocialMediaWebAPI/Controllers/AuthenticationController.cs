74763fe737957e8009d45028756ee5874c05a2d0
David La Grange <davidnlagrange@hotmail.com>
2021-05-12 15:52:01 -0500

    Created AuthenticationController + Injected User Manager Via DI

---
+++ SocialMediaWebAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialMediaWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        public AuthenticationController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+    }
+}
